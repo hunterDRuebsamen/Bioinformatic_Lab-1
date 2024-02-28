d9b1dbebc2ef2135c5bd7c480ff5b3ef02195424
Hunter <34562875+WheeledParasite@users.noreply.github.com>
2024-02-28 10:55:18 -0800

    Samples can now be read in from folders

--- methyl_filter.py
+++ methyl_filter.py
@@ -31,11 +31,16 @@ def read_sample_file(sample_file_path: str):
     Reads the 'Bismark coverage' .cov.gz file from the mouse tissue sample
 
     Returns:
-    - pd.DataFrame: methylation data for mouse tissue sample
+    - df (pd.DataFrame): methylation data for mouse tissue sample
     """
+    sample_files = []
+    for filename in os.listdir(sample_file_path):    
+            sample_files.append(filename)
+    return sample_files
 
-    reader = Cov_Read()
-    return reader.build_df(sample_file_path)
+    
+    # reader = Cov_Read()
+    # return reader.build_df(sample_file_path)
 
 
 def filter_dataframe(df: pd.DataFrame, name_str: str, chromosome: str, start_loc: int, end_loc: int) -> pd.DataFrame:
@@ -52,11 +57,11 @@ def filter_dataframe(df: pd.DataFrame, name_str: str, chromosome: str, start_loc
     - pd.DataFrame: Filtered DataFrame.
     """
     # Apply the filter conditions, ensuring the range is inclusive
-    filtered_df = df[(df['chromosome'] == str(chromosome)) &
+    filtered_df = df[(df['chromosome'] == chromosome) &
                      (df['s_loc'] >= start_loc) &
                      (df['s_loc'] <= end_loc)].copy() # Use copy() to defined filtered_df as copy, not view and avoid SettingWithCopyWarning
     
-    # Trim CG_Sites from file dump
+    # Trim CG_Sites column from file dump
     filtered_df.drop(labels= 'CG site', axis=1, inplace= True)
 
     # TODO: change cov_read_module to simply not read past the first 2 digits rather than round here
@@ -64,7 +69,7 @@ def filter_dataframe(df: pd.DataFrame, name_str: str, chromosome: str, start_loc
 
     # set name column equal to single value
     filtered_df['name'] = name_str
-
+ 
     column_order = ['name', 'chromosome', 's_loc', 'e_loc',
                     'methyl rate', 'methylated reads', 'unmethylated reads']
     
@@ -77,20 +82,30 @@ def main():
     parser = argparse.ArgumentParser(
                     prog='methyl_filter',
                     description='returns methylation level of locations in a specified range and chromosome')
-    parser.add_argument('SampleFilePath')
+    parser.add_argument('SampleDirectoryPath')
     parser.add_argument('SettingFile')
     args = parser.parse_args()
     
-    cov_df = read_sample_file(args.SampleFilePath)
+
+    samples_directory = read_sample_file(args.SampleDirectoryPath)
     settings_df = read_setting_file(args.SettingFile)
-    
+
+    reader = Cov_Read()
+
+    '''
+    For each region in the settings_file, 
+    generate an output for each sample in the sample directory
+    '''
     for row in settings_df.itertuples(index=True, name='Pandas'):
         name = row.name
         chromosome = str(row.chromosome)
         start_loc = int(row.start_loc)
         end_loc = int(row.end_loc)
 
-        filter_dataframe(cov_df, name, chromosome, start_loc, end_loc).to_csv(f'{name}_out.csv', index=False, header=True)
+        for idx, sample in enumerate(samples_directory):
+            final_path = f"{args.SampleDirectoryPath}/{sample}"
+            cov_df = reader.build_df(final_path)
+            filter_dataframe(cov_df, name, chromosome, start_loc, end_loc).to_csv(f'{name}_out_{idx+1}.csv', index=False, header=True)
 
         # with pd.option_context('display.max_rows', None, 'display.max_columns', None):
         #     print(filter_dataframe(cov_df, name, chromosome, start_loc, end_loc).to_csv(index=False, header=False))
