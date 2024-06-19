c75f5805409277999bcc4a5e10483031618053de
Hunter <34562875+WheeledParasite@users.noreply.github.com>
2024-06-19 12:23:11 -0700

    updated requirements and changed cpg_island script

--- cpg_islands_to_settings.py
+++ cpg_islands_to_settings.py
@@ -3,27 +3,27 @@ from io import StringIO
 
 
 # Reading the data into a DataFrame
-df = pd.read_csv("CpgIsland-GRCm39.txt", sep='\t')
+df = pd.read_csv("cpgIsland-GRCm38.txt", sep='\t')
 
 df = df.drop(columns=["name"])
 
 # Renaming columns
 df = df.rename(columns={
-    "#bin": "name",
+    "#bin": "#probe",
     "chrom": "chromosome",
     "chromStart": "start_loc",
     "chromEnd": "end_loc"
 })
 
 # Dropping unnecessary columns
-df = df[['name', 'chromosome', 'start_loc', 'end_loc']]
+df = df[['#probe', 'chromosome', 'start_loc', 'end_loc']]
 
 # Removing the "chr" from the start of every entry in the chromosome column
 df['chromosome'] = df['chromosome'].str.replace('chr', '')
 
-df['name'] = df.apply(lambda row: f"{row.name}-{row['name']}", axis=1)
+df['#probe'] = df.apply(lambda row: f"{row.name}-{row['#probe']}", axis=1)
 
 
-df.to_csv('CpgIsland-GRCm39-settings.txt', index=False)
+df.to_csv('cpgIsland-GRCm38-settings.txt', index=False)
 
-print("Saved CpgIsland-GRCm39-settings.txt to file")
+print("Saved CpgIsland-GRCm38-settings.txt to file")
